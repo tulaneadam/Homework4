f75197830bed70444bc5cd5274471078d09eaee3
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-20 15:01:23 -0700

    definig functions and links to pages

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function beginQuiz(){
     console.log("beginQuiz")
+    window.location.href = "/Question1"
+}
+
+function wrongAnswer() {
+    alert("You Chose The Wrong Answer, Please Try Again!")
+    
+}
+
+function correctAnswer1() {
+    window.location.href = "/Question2"
+}
+
+function correctAnswer2() {
+    window.location.href = "/Question3"
+}
+
+function correctAnswer3() {
+    window.location.href = "/Question4"
+}
+
+function correctAnswer4() {
+    window.location.href = "/Alldone"
 }
