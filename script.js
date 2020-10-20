51120ce60cc14482813321fa7dd32e8e3750fb04
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-20 15:10:35 -0700

    updating code

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ function wrongAnswer() {
 }
 
 function correctAnswer1() {
+    alert("You're Answer is Correct. Here's You're Next Question.")
     window.location.href = "/Question2"
 }
 
 function correctAnswer2() {
+    alert("You're Answer is Correct. Here's You're Next Question.")
     window.location.href = "/Question3"
 }
 
 function correctAnswer3() {
+    alert("You're Answer is Correct. Here's You're Next Question.")
     window.location.href = "/Question4"
 }
 
 function correctAnswer4() {
+    alert("You're Answer is Correct. Great Job!!")
     window.location.href = "/Alldone"
 }
